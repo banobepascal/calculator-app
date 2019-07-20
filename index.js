676f1d478d72cfbb166a4729369b9eb195a55de6
banobepascal <banobepascal@gmail.com>
2019-07-20 17:13:43 +0300

    choose operation

--- index.js
+++ index.js
@@ -32,7 +32,25 @@ class Calculator {
 	}
 
 	compute(){
-        
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return 
+        switch (this.operation){
+            case '+':
+            computation = prev + current
+            break;
+            case '-':
+            computation = prev - current
+            break;
+            case '*':
+            computation = prev * current
+            break;
+            case '÷':
+            computation = prev / current
+            break;
+        }
+
 	}
 
 	updateDisplay(){
