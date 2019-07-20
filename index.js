4d239d0518e5356b3233f7120164c3b5d8041d91
banobepascal <banobepascal@gmail.com>
2019-07-20 17:07:13 +0300

    choose operation

--- index.js
+++ index.js
@@ -22,15 +22,22 @@ class Calculator {
 	}
 
 	chooseOperation(operation){
-
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
 	}
 
 	compute(){
-
+        
 	}
 
 	updateDisplay(){
 		this.currentOperandTextElement.innerText  = this.currentOperand;
+		this.previousOperandTextElement.innerText  = this.previousOperand;
 	}
 }
 
@@ -58,3 +65,8 @@ operationButtons.forEach(button => {
         calculator.updateDisplay();
     });
 });
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
